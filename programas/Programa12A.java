c5024fc6d08ce9b2fb885d4c6002580f0027aa67
dVazquezAntonella <47192360+dVazquezAntonella@users.noreply.github.com>
2019-02-01 12:12:38 -0600

    Add files via upload

--- programas/Programa12A.java
+++ programas/Programa12A.java
@@ -1,10 +1,35 @@
 package programas;
 
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class Programa12A {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner leer = new Scanner(System.in);
+		
+		int c=0,m=0;
+		String R;
+		do{
+			System.out.println("Precio(entre 0 y 400)");
+	     float pre= leer.nextFloat();		
+			if(pre<201){
+				c++;
+				}
+			if(pre>199 & pre<401){
+				m++;
+			}
+			
+			System.out.println("¿Desea registar mas productos? ");
+			R= leer.next();
+		}while(R.equals("S"));
+		
+		
+		
+		
+		System.out.println("La cantidad de ventas menores a 200 fue: "+c);
+		System.out.println("La cantidad de ventas mayores a 200 fue: "+m);
+		
 	}
-
 }
